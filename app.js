a1188932a68423aa5633d4881c86bfc8f60af569
skoms <45171368+skoms@users.noreply.github.com>
2021-03-19 15:45:59 +0100

    initialize base error handeling and add few templates and routes

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { sequelize } =  require('./models');
+
+// Authenticates and syncs the database and logs whether is was a success or failure
+sequelize.authenticate()
+  .then(() => {
+   console.log("'Sequelize' has established connection with 'SQLite' successfully.");
+   sequelize.sync();
+   console.log(path.join(__dirname, 'public'));
+  })
+  .catch(err => {
+   console.log("'Sequelize' was unable to connect to the database: ", err);
+  });
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,31 +24,34 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static',express.static(path.join(__dirname, 'public'))); // '/static/dir-name/file-name' to access static files
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const pageNotFound = new Error();
+  pageNotFound.status = 404;
+  pageNotFound.message = 'Sorry, the page you were looking for does not exist! Please try again.';
+  res.render('page-not-found', { title: "Page Not Found", error: pageNotFound });
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  err.status = err.status || 500;
+  err.message = err.message || "A server issue has occured, please try refresh the page.";
+  
+  console.log(`Status: ${err.status} | Message: ${err.message}`);
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: "Server Error", err: err });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const { Book } = require('../models');
+
+/* Async handler Middleware. */
+function asyncHandler(callback){
+  return async(req, res, next) => {
+    try {
+      await callback(req, res, next)
+    } catch(error){
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler((req, res, next) => {
+ res.redirect('/books');
+}));
+
+/* GET books page. */
+router.get('/books', asyncHandler((req, res, next) => {
+  // res.render('index', { title: 'Express' });
+  const books = Book.findAll();
+  res.render('all_books', {});
+ }));
+ 
 
 module.exports = router;
